153748e9c07152bc4c8c9f16fe76a527216ff9da
ansungho22 <ansungho22@gmail.com>
2025-01-21 12:03:52 +0900

    edu-goods /configmap 추가

--- src/main/java/com/example/demo/controller/GoodsController.java
+++ src/main/java/com/example/demo/controller/GoodsController.java
@@ -1,20 +1,29 @@
 package com.example.demo.controller;
 
 
+import com.example.demo.dto.DataSourceConfig;
 import com.example.demo.dto.GoodsDto;
 import com.example.demo.service.GoodsService;
+import lombok.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 //@RequestMapping("/api/v1/goods")
 public class GoodsController {
     private final GoodsService goodsService;
+    private final DataSourceConfig dataSourceConfig;
 
-    public GoodsController(GoodsService goodsService) {
+
+
+    public GoodsController(GoodsService goodsService, DataSourceConfig dataSourceConfig) {
         this.goodsService = goodsService;
+        this.dataSourceConfig = dataSourceConfig;
     }
 
     @GetMapping("/api/v1/goods/{goodsNo}")
@@ -25,5 +34,14 @@ public class GoodsController {
     @GetMapping("/")
     public String healthCheck() {
         return "ok";
+    }
+
+    @GetMapping("/configmap")
+    public Map<String, String> getConfigMap() {
+        Map<String, String> configMap = new HashMap<>();
+        configMap.put("hostip", dataSourceConfig.getHostip());
+        configMap.put("username", dataSourceConfig.getUsername());
+        configMap.put("password", dataSourceConfig.getPassword());
+        return configMap;
     }
 }
